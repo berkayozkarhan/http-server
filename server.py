1cee36f12b81333840d2dcb06c82a23428dd0056
Berkay Özkarhan <berkayozkarhan99@gmail.com>
2021-04-19 18:51:48 +0300

    Tasarım değiştirildi.

--- server.py
+++ server.py
@@ -9,22 +9,33 @@ import cgi
 
 tasklist = ['Task 1','Task 2','Task 3']
 
+def load_binary(file):
+    with open(file, 'rb') as file:
+        return file.read()
+
+
 class requestHandler(BaseHTTPRequestHandler): #BaseHTTPRequestHandler sınıfından kalıtım ile alınıyor.
     def do_GET(self):
         print(self.path)
 
         try:
             filepath =  self.path[1:] #request ile gelen dosya yolu.başında '/' işareti ile geldiği için / değerini almıyorum.
-            file = codecs.open(filepath, "r", "utf-8")
+            mimetype, _ = mimetypes.guess_type(filepath)
+            if mimetype == 'image/png' or mimetype == 'image/jpeg':
+                file = load_binary(filepath)
+                self.send_response(200)
+                self.send_header('Content-type',mimetype)
+                self.end_headers()
+                self.wfile.write(file)
+            else:
+                file = codecs.open(filepath, "r", "utf-8")
+                self.send_response(200)
+                self.send_header('Content-type', mimetype)
+                self.end_headers()
+                self.wfile.write(file.read().encode())
         except IOError:
             self.send_error(404, 'File Not Found: %s ' % filepath)
             return
-        self.send_response(200)
-        output = file.read()
-        mimetype, _ = mimetypes.guess_type(filepath)
-        self.send_header('Content-type', mimetype)
-        self.end_headers()
-        self.wfile.write(output.encode())
 
 
 
